bb43d8e848632e2d8f6f92e0dd669fcdb4b88e1f
DannylSyph3r <dopedanny001@gmail.com>
2025-11-12 21:53:56 +0100

    Fix(UserService|PushNotifications|ApiGateway: Env configs and build fixes)

--- PushNotificationService/src/main/resources/static/firebase-messaging-sw-example.js
+++ PushNotificationService/src/main/resources/static/firebase-messaging-sw-example.js
@@ -1,7 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-const firebaseConfig = {};
+const firebaseConfig = __FIREBASE_CONFIG_JSON_PLACEHOLDER__;
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
