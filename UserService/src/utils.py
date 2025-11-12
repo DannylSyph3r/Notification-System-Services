76afe6220634d1c76060cf6853e91624fd26f90e
DannylSyph3r <dopedanny001@gmail.com>
2025-11-12 22:22:38 +0100

    Fix(Push|JWT): Build file updated for notification, tweak JWT base64 conversion

--- UserService/src/utils.py
+++ UserService/src/utils.py
@@ -1,12 +1,16 @@
 import os
 from datetime import datetime, time, timedelta, timezone
+import base64
 import jwt
 from dotenv import load_dotenv
 from src.schemas import TokenResponse
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "your secret key")
+
+SECRET_KEY_ENCODED = os.getenv("SECRET_KEY", "your secret key")
+SECRET_KEY = base64.b64decode(SECRET_KEY_ENCODED)
+
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
 
@@ -26,4 +30,3 @@ async def generate_token(payload: dict, expire_delta: int | None = None) -> Toke
     token_response = TokenResponse(token=encoded_token)
 
     return token_response
-
