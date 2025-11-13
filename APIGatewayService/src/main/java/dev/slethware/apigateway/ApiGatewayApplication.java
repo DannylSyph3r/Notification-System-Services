f7b49faf180cb9e466c24b69d103c3ed8c4a0987
DannylSyph3r <dopedanny001@gmail.com>
2025-11-13 20:32:12 +0800

    Chore(Documentation): OpenAPI documentation for API Gateway

--- APIGatewayService/src/main/java/dev/slethware/apigateway/ApiGatewayApplication.java
+++ APIGatewayService/src/main/java/dev/slethware/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,40 @@
 package dev.slethware.apigateway;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				contact = @Contact(
+						name = "Akinola Daniel",
+						email = "danieleakinola@gmail.com"
+				),
+				description = "OpenApi documentation for Distributed Notification System",
+				title = "Distributed Notification Documentation",
+				version = "1.0",
+				license = @License(
+						name = "Licence name",
+						url = "https://example.com"
+				),
+				termsOfService = "Terms of service"
+		)
+)
+@SecurityScheme(
+		name = "bearerAuth",
+		description = "JWT auth description",
+		scheme = "bearer",
+		type = SecuritySchemeType.HTTP,
+		bearerFormat = "JWT",
+		in = SecuritySchemeIn.HEADER
+)
 @SpringBootApplication
 public class ApiGatewayApplication {
 
